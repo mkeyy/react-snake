9073f6db33e934ec9835cee2cb40adbd996620f2
mkeyy <mk.websitesdev@gmail.com>
2019-01-12 23:30:12 +0100

    improved basic theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 const App = () => (
-  <div>
+  <div className="container">
     <h1>React Lightweight</h1>
+    <p>
+      Lightweight starter kit for creating apps with React. More on{" "}
+      <a
+        href="https://github.com/mkeyy/react-lightweight"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </a>
+      .
+    </p>
   </div>
 );
 
